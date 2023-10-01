fa934e139d437a059e594d9c7c21cfbcbfed4051
Krystian Fowler <krystian@stratokit.io>
2023-10-01 19:11:34 +0200

    274. H-Index

---
+++ array/274_h-index.py
@@ -0,0 +1,10 @@
+# Space complexity: O(1)
+# Time complexity: O(nlogn)
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort() # O(nlogn)
+        hIdx = 0
+        for idx, n in enumerate(citations): # O(n)
+            if len(citations) - idx >= n:
+                hIdx = n
+        return hIdx
