ac7e474b49b0d6537ec339d18c1c6a8ca64e7393
Krystian Fowler <krystian@stratokit.io>
2023-10-01 08:38:11 +0200

    88. Merge Sorted Array

---
+++ array/88_merge_sorted_array.py
@@ -0,0 +1,16 @@
+# Space complexity: O(1)
+# Time complexity: O(m + n)
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        n1Idx = m - 1
+        n2Idx = n - 1
+        for i in range(m + n - 1 , -1, -1):
+            if n2Idx < 0 or nums1[n1Idx] > nums2[n2Idx]:
+                nums1[i] = nums1[n1Idx]
+                n1Idx -= 1
+            else: 
+                nums1[i] = nums2[n2Idx]
+                n2Idx -= 1
